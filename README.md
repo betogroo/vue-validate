10cfd53588ed617f07237206a77e68ab37b1c640
Luiz Humberto <luizhumberto@gmail.com>
2023-10-15 09:39:18 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# essentials
+
+## Project setup
+
+Clone project:
+
+```
+https://github.com/betogroo/vue-scaffold.git
+```
+
+Install all Dependencies:
+
+```
+npm i
+```
+
+Prepare Husky:
+
+```
+npm run prepare
+```
+
+Run:
+
+```
+npm run dev
+```
---
+++ src/modules/main/views/AboutView.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { version as vuetifyVersion } from 'vuetify'
+import { version as vueVersion } from 'vue'
+import { useMainStore } from '../store/useMainStore'
+const store = useMainStore()
+</script>
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="align-center text-center fill-height">
+      <v-row>
+        <v-col>
+          <v-img src="@/assets/vue.svg" />
+          {{ vueVersion }}
+        </v-col>
+        <v-col>
+          <v-img src="@/assets/logo.png" />
+          {{ vuetifyVersion }}
+        </v-col>
+
+        <v-col cols="12">
+          <div class="text-body-1">Testing Store</div>
+
+          <h5 class="text-h6">
+            The HomeView counter value is {{ store.counter }}
+          </h5>
+        </v-col>
+      </v-row>
+
+      <div class="py-14" />
+    </v-responsive>
+  </v-container>
+</template>
