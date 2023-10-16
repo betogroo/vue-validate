b87dcf618d0bc7e5218419c6a415509772be548c
betogroo <luizhumberto@gmail.com>
2023-10-16 17:54:53 -0300

    feat: add name textfield

--- src/modules/main/views/VuelidateView.vue
+++ src/modules/main/views/VuelidateView.vue
@@ -1,23 +1,26 @@
 <script setup lang="ts">
-import { reactive, ref } from 'vue'
+import { computed, reactive, ref } from 'vue'
 import { useVuelidate } from '@vuelidate/core'
-import { required, numeric, email } from '@vuelidate/validators'
+import { required, numeric, email, minLength } from '@vuelidate/validators'
 
 /* const state = reactive({
   price: '',
 }) */
 
 const formData = ref({
+  name: '',
   price: '',
   email: '',
 })
 
 const rules = {
+  name: { required, minLength: minLength(6) },
   price: { required, numeric },
   email: { required, email },
 }
 
 const v$ = useVuelidate(rules, formData.value)
+
 const onSubmit = async () => {
   const validForm = await v$.value.$validate()
   if (!validForm) return
@@ -26,43 +29,42 @@ const onSubmit = async () => {
 </script>
 
 <template>
-  <v-container class="d-flex align-start justify-center-fill-height">
-    <v-responsive max-width="500">
+  <v-container class="d-flex align-start justify-center fill-height">
+    <v-responsive max-width="640">
       <h1 class="text-red">Vuelidate</h1>
-      <v-card class="pa-1">
+      <v-card class="pa-4">
         <v-form @submit.prevent="onSubmit">
-          <v-row>
-            <v-col cols="12">
-              <v-text-field
-                v-model="v$.price.$model"
-                :error-messages="
-                  v$.price.$errors.map((e) => e.$message.toString())
-                "
-                variant="outlined"
-                @blur="v$.price.$touch"
-                @update:model-value="v$.price.$touch"
-              />
-            </v-col>
-            <v-col cols="12">
-              <v-text-field
-                v-model="v$.email.$model"
-                :error-messages="
-                  v$.email.$errors.map((e) => e.$message.toString())
-                "
-                variant="outlined"
-                @blur="v$.email.$touch"
-                @update:model-value="v$.email.$touch"
-              />
-            </v-col>
-            <v-col cols="12">
-              <v-btn
-                color="success"
-                type="submit"
-                variant="outlined"
-                >Enviar</v-btn
-              ></v-col
+          <v-text-field
+            v-model="v$.name.$model"
+            :error-messages="v$.name.$errors.map((e) => e.$message.toString())"
+            variant="outlined"
+            @blur="v$.name.$touch"
+            @update:model-value="v$.name.$touch"
+            ><template #message="{ message }"
+              ><small>{{ message }}</small></template
             >
-          </v-row>
+          </v-text-field>
+          <v-text-field
+            v-model.number="v$.price.$model"
+            :error-messages="v$.price.$errors.map((e) => e.$message.toString())"
+            type="number"
+            variant="outlined"
+            @blur="v$.price.$touch"
+            @update:model-value="v$.price.$touch"
+          />
+          <v-text-field
+            v-model="v$.email.$model"
+            :error-messages="v$.email.$errors.map((e) => e.$message.toString())"
+            variant="outlined"
+            @blur="v$.email.$touch"
+            @update:model-value="v$.email.$touch"
+          />
+          <v-btn
+            color="success"
+            type="submit"
+            variant="outlined"
+            >Enviar</v-btn
+          >
         </v-form>
       </v-card>
     </v-responsive>
