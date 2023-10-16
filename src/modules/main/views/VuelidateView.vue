df61c335c43ced5422a329f9cd9b3c06e9117de7
betogroo <luizhumberto@gmail.com>
2023-10-16 13:41:29 -0300

    chore: add vuelidate-route

---
+++ src/modules/main/views/VuelidateView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <v-container class="d-flex align-start justify-center-fill-height">
+    <v-responsive max-width="800">
+      <h1 class="text-red">Vuelidate</h1>
+    </v-responsive>
+  </v-container>
+</template>
