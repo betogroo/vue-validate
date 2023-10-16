2e32d748a0191478e8c0b26f1487098bf8439f6f
betogroo <luizhumberto@gmail.com>
2023-10-16 10:59:59 -0300

    feat: add on submit function

--- src/modules/main/views/VeeView.vue
+++ src/modules/main/views/VeeView.vue
@@ -1,14 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const password = ref('')
+const onSubmit = () => {
+  console.log(password.value)
+}
+</script>
 
 <template>
   <v-container class="d-flex align-start justify-center fill-height">
     <v-responsive max-width="800">
       <v-card>
         <v-card-title>Vee Validate</v-card-title>
-        <v-form>
+        <v-form @submit.prevent="onSubmit">
           <v-row>
             <v-col cols="12"
               ><v-text-field
+                v-model="password"
                 counter
                 density="compact"
                 hint="Mínimo de 8 caracteres"
@@ -18,7 +26,9 @@
                 variant="outlined"
             /></v-col>
             <v-col></v-col>
-            <v-col></v-col>
+            <v-col cols="12">
+              <v-btn type="submit">Enviar</v-btn>
+            </v-col>
           </v-row>
         </v-form>
       </v-card>
