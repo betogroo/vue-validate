3ff403c7b1ed5a8e2033f0d5cf60c2d70b9183b6
betogroo <luizhumberto@gmail.com>
2023-10-17 15:51:19 -0300

    feat: add fild name with validators

--- src/modules/main/views/VeeView.vue
+++ src/modules/main/views/VeeView.vue
@@ -10,49 +10,56 @@ const validationSchema = toTypedSchema(
       .string()
       .min(1, 'Campo obrigatório')
       .min(8, 'No mínimo 8 dígitos'),
+    name: z
+      .string()
+      .min(1, 'Campo obrigatório')
+      .min(3, 'Mínimo de 3 caracteres'),
   }),
 )
 
-const { handleSubmit, handleReset, meta } = useForm({ validationSchema })
+const { handleSubmit, meta, values } = useForm({
+  validationSchema,
+})
 
+const name = useField('name', validationSchema)
 const password = useField('password', validationSchema)
 
 const onSubmit = handleSubmit(async () => {
-  console.log(meta.value.valid)
+  console.log(values)
 })
 </script>
 
 <template>
   <v-container class="d-flex align-start justify-center fill-height">
     <v-responsive max-width="800">
-      <v-sheet width="250">
-        <code> {{ password.value }} {{ meta }} </code>
-      </v-sheet>
       <v-card>
         <v-card-title>Vee Validate</v-card-title>
         <v-form @submit.prevent="onSubmit">
-          <v-row>
-            <v-col cols="12"
-              ><v-text-field
-                v-model="password.value.value"
-                counter
-                density="compact"
-                :error-messages="password.errorMessage.value"
-                hint="Mínimo de 8 caracteres"
-                label="Senha"
-                placeholder="Digit sua senha"
-                type="password"
-                variant="outlined"
-            /></v-col>
-            <v-col></v-col>
-            <v-col cols="12">
-              <v-btn
-                :disabled="!meta.valid"
-                type="submit"
-                >Enviar</v-btn
-              >
-            </v-col>
-          </v-row>
+          <v-text-field
+            v-model="name.value.value"
+            density="compact"
+            :error-messages="name.errorMessage.value"
+            label="Nome"
+            placeholder="Digite sua senha"
+            type="text"
+            variant="outlined"
+          />
+          <v-text-field
+            v-model="password.value.value"
+            counter
+            density="compact"
+            :error-messages="password.errorMessage.value"
+            hint="Mínimo de 8 caracteres"
+            label="Senha"
+            placeholder="Digit sua senha"
+            type="password"
+            variant="outlined"
+          />
+          <v-btn
+            :disabled="!meta.valid"
+            type="submit"
+            >Enviar</v-btn
+          >
         </v-form>
       </v-card>
     </v-responsive>
