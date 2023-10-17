f722d48ce1fc882ee0a1d93e899c7a29a25c7fc8
betogroo <luizhumberto@gmail.com>
2023-10-17 13:40:16 -0300

    chore: create password validation

--- src/modules/main/views/VeeView.vue
+++ src/modules/main/views/VeeView.vue
@@ -1,24 +1,41 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import { useField, useForm } from 'vee-validate'
+import { object, z } from 'zod'
+import { toTypedSchema } from '@vee-validate/zod'
 
-const password = ref('')
-const onSubmit = () => {
-  console.log(password.value)
-}
+const validationSchema = toTypedSchema(
+  object({
+    password: z
+      .string()
+      .min(1, 'Campo obrigatório')
+      .min(8, 'No mínimo 8 dígitos'),
+  }),
+)
+
+const { handleSubmit, handleReset } = useForm({ validationSchema })
+
+const password = useField('password', validationSchema)
+
+const onSubmit = handleSubmit(async () => {
+  console.log(password.value.value)
+})
 </script>
 
 <template>
   <v-container class="d-flex align-start justify-center fill-height">
     <v-responsive max-width="800">
+      {{ password.value }}
       <v-card>
         <v-card-title>Vee Validate</v-card-title>
         <v-form @submit.prevent="onSubmit">
           <v-row>
             <v-col cols="12"
               ><v-text-field
-                v-model="password"
+                v-model="password.value.value"
                 counter
                 density="compact"
+                :error-messages="password.errorMessage.value"
                 hint="Mínimo de 8 caracteres"
                 label="Senha"
                 placeholder="Digit sua senha"
